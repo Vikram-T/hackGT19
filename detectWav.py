e0c35175cbbd9d90e126139bb73cac02cdcf7270
Vivek Mhatre <vmhatre@ucvts.tec.nj.us>
2019-10-26 18:12:41 -0400

    day two
    Mp3toText is actual file to convert mp3 to text. Authentication needed.

--- detectWav.py
+++ detectWav.py
@@ -1,6 +1,9 @@
 import os
 import wave
-file_name = r"C:\Users\vivek\Dropbox (GaTech)\Projects\HackGT\Interpreting line plots.wav"
+file_name = r"C:\Users\vivek\Dropbox (GaTech)\Projects\HackGT\Interpreting_line_plots.wav"
 wave_file = wave.open(file_name, "rb")
 frame_rate = wave_file.getframerate()
+channels = wave_file.getnchannels()
 print(frame_rate)
+print(channels)
+
